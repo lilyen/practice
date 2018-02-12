98791cac4a7d0df8f9902cca27ce73a992549f90
lily.yen79 <lily.yen79@TI03NB61>
2018-02-12 14:43:22 +0800

    predicate

--- practice/program/judgmentNumber.cs
+++ practice/program/judgmentNumber.cs
@@ -1,11 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace practice.program
 {
-    public class judgmentNumber
+    public class JudgmentNumber
     {
+        private Dictionary<Predicate<int>, int> MyDic = new Dictionary<Predicate<int>, int>
+        {
+            {(i => i>0 && i<=1), 1},
+            {(i => i>1 && i<=3), 2},
+            {(i => i>3 && i<=5), 3},
+            {(i => i>5 && i<=24), 4},
+            {(i => i>24), 5}
+        };
+
+        public int ChangeNumber(int num)
+        {
+            var arr = new[] { 0, 1, 3, 5, 24 };
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (num > arr[i] && num <= arr[i + 1])
+                {
+                    return i + 1;
+                }
+            }
+            return 5;
+        }
+
+        public int ChangeNumber_2(int num)
+        {
+            foreach (KeyValuePair<Predicate<int>, int> item in MyDic)
+            {
+                if (item.Key.Invoke(num))
+                {
+                    return item.Value;
+                }
+            }
+            return 5;
+        }
+
+        public int ChangeNumber_3(int num)
+        {
+            //return MyDic[MyDic.Keys.Where(m => m.Invoke(num)).First()];
+            return MyDic.FirstOrDefault(m => m.Key(num)).Value;
+        }
+
+        public bool testPredicate()
+        {
+            Predicate p = new Predicate((i => i >= 0 && i < 1));
+            return p.Invoke(0);
+        }
+
+        public delegate bool Predicate(int target);
     }
 }
---
+++ practiceTest/JudgmentNumberTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using practice.program;
+
+namespace practiceTest
+{
+    [TestClass]
+    public class JudgmentNumberTest
+    {
+        JudgmentNumber target = new JudgmentNumber();
+
+        [TestMethod]
+        public void ChangeNumber()
+        {
+            Assert.AreEqual(2, target.ChangeNumber(3));
+            Assert.AreEqual(1, target.ChangeNumber(1));
+            Assert.AreEqual(4, target.ChangeNumber(6));
+            Assert.AreEqual(5, target.ChangeNumber(25));
+        }
+
+        [TestMethod]
+        public void ChangeNumber_2()
+        {
+            Assert.AreEqual(2, target.ChangeNumber_2(3));
+            Assert.AreEqual(1, target.ChangeNumber_2(1));
+            Assert.AreEqual(4, target.ChangeNumber_2(6));
+            Assert.AreEqual(5, target.ChangeNumber_2(25));
+        }
+
+        [TestMethod]
+        public void ChangeNumber_3()
+        {
+            Assert.AreEqual(2, target.ChangeNumber_3(3));
+            //Assert.AreEqual(1, target.ChangeNumber_2(1));
+            //Assert.AreEqual(4, target.ChangeNumber_2(6));
+            //Assert.AreEqual(5, target.ChangeNumber_2(25));
+        }
+
+        [TestMethod]
+        public void testPredicate()
+        {
+            Assert.IsTrue(target.testPredicate());
+        }
+    }
+}
